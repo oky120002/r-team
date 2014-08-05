e64e16757eaf0dac5ddc8b5dcad5ade973f770c1
何雨 <heyu.520@qq.com>
2014-08-05 18:03:42 +0800

    1234

--- r-apps/r-apps-taobaoshua/src/main/java/com/r/app/taobaoshua/TaoBaoApp.java
+++ r-apps/r-apps-taobaoshua/src/main/java/com/r/app/taobaoshua/TaoBaoApp.java
@@ -1,7 +1,16 @@
 package com.r.app.taobaoshua;
 
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.r.app.taobaoshua.view.TaoBaoLoginFrame;
 import com.r.core.desktop.ctrl.HCtrlUtil;
 
 /**
@@ -9,7 +18,40 @@ import com.r.core.desktop.ctrl.HCtrlUtil;
  * 
  */
 public class TaoBaoApp {
+
+    private static TaoBaoApp app;
+    private ApplicationContext applicationContext; // spring容器
+    private TaoBaoLoginFrame loginFrame; // 登陆窗口
+
+    /** 获取ApplicationContext */
+    public ApplicationContext getApplicationContext() {
+        return this.applicationContext;
+    }
+
+    /** 返回唯一实例 */
+    public static final TaoBaoApp getTaoBaoApp() {
+        return TaoBaoApp.app;
+    }
     
+    /** 获取淘宝刷图标 */
+    public static Image getDefaultIco() throws IOException {
+        String imagepath = "com/r/app/taobaoshua/image/tray.png";
+        InputStream ins = ClassLoader.getSystemResourceAsStream(imagepath);
+        return ImageIO.read(ins);
+    }
+
+    /** 启动 */
+    private static void startup() {
+        TaoBaoApp.app = new TaoBaoApp();
+
+        // 初始化spring容器
+        TaoBaoApp.app.applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+
+        // 实例化登陆窗口
+        TaoBaoApp.app.loginFrame = new TaoBaoLoginFrame();
+        TaoBaoApp.app.loginFrame.setVisible(true);
+    }
+
     public static void main(String[] args) {
         HCtrlUtil.setNoSpot();
         HCtrlUtil.setUIFont(null);
@@ -17,7 +59,7 @@ public class TaoBaoApp {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                // TaobaoShuaApp.startup(); // 启动
+                TaoBaoApp.startup(); // 启动
             }
         });
     }
---
+++ r-apps/r-apps-taobaoshua/src/main/java/com/r/app/taobaoshua/view/TaoBaoLoginFrame.java
@@ -0,0 +1,62 @@
+package com.r.app.taobaoshua.view;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import com.r.app.taobaoshua.TaoBaoApp;
+import com.r.core.desktop.ctrl.HBaseFrame;
+import com.r.core.log.Logger;
+import com.r.core.log.LoggerFactory;
+
+/**
+ * 淘宝刷 登陆窗口
+ * 
+ * @author oky
+ * 
+ */
+public class TaoBaoLoginFrame extends HBaseFrame implements ActionListener {
+    private static final long serialVersionUID = -7265881767861403989L;
+    private static final Logger logger = LoggerFactory.getLogger(TaoBaoLoginFrame.class);
+    
+    
+    public TaoBaoLoginFrame() {
+        super("淘宝刷-登陆");
+        logger.info("MainDesktop newInstance .............");
+        initStyle();
+        initComponents();
+        initListeners();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+    private void initStyle() {
+        setSize(new Dimension(350, 220));// 设置窗口大小
+        setResizable(false);
+        setLocationRelativeTo(null); // 移动到屏幕中部(上下左右)
+        try {
+            setIconImage(TaoBaoApp.getDefaultIco());
+        } catch (Exception e) {
+        }
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+
+    private void initComponents() {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+
+    private void initListeners() {
+        // TODO Auto-generated method stub
+        
+    }
+}
