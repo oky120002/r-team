855ccb4a0bb3894de0acafee22cd6593259e143d
rain <rain@localhost>
2015-01-05 14:42:15 +0800

    bug修正

--- r-app/src/main/java/com/r/app/taobaoshua/bluesky/dao/TaskDaoImpl.java
+++ r-app/src/main/java/com/r/app/taobaoshua/bluesky/dao/TaskDaoImpl.java
@@ -30,145 +30,145 @@ import com.r.core.log.LoggerFactory;
  */
 @Repository("taskDao")
 public class TaskDaoImpl extends AbstractDaoImpl<Task> implements TaskDao {
-	private static final Logger logger = LoggerFactory.getLogger(TaskDaoImpl.class); // 日志
-	private static final String BODY_ENCODE = "gb2312";
-	private static final String POST_ENCODE = "gb2312";
+    private static final Logger logger = LoggerFactory.getLogger(TaskDaoImpl.class); // 日志
+    private static final String BODY_ENCODE = "gb2312";
+    private static final String POST_ENCODE = "gb2312";
 
-	private HttpSocket httpSocket = HttpSocket.newHttpSocket(true, null);
+    private HttpSocket httpSocket = HttpSocket.newHttpSocket(true, null);
 
-	public TaskDaoImpl() {
-		super(Task.class);
-		logger.info("TaskDaoImpl Instance............................");
-		httpSocket.setTimeout(60_000); // 1分钟超时
-	}
+    public TaskDaoImpl() {
+        super(Task.class);
+        logger.info("TaskDaoImpl Instance............................");
+        httpSocket.setTimeout(60_000); // 1分钟超时
+    }
 
-	@Override
-	public Session getSession() {
-		return super.getSession();
-	}
+    @Override
+    public Session getSession() {
+        return super.getSession();
+    }
 
-	/** 设置当前链接的代理 */
-	@Override
-	public void setSocketProxy(HttpProxy httpProxy) {
-		this.httpSocket.setProxy(httpProxy);
-	}
+    /** 设置当前链接的代理 */
+    @Override
+    public void setSocketProxy(HttpProxy httpProxy) {
+        this.httpSocket.setProxy(httpProxy);
+    }
 
-	/** 获取验证码图片 */
-	@Override
-	public Image getLoginCaptchaImage() {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Referer", "http://www2.88sxy.com/user/login/");
-		ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/plus/verifycode.asp?n=", map);
-		return responseHeader.bodyToImage();
-	}
+    /** 获取验证码图片 */
+    @Override
+    public Image getLoginCaptchaImage() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Referer", "http://www2.88sxy.com/user/login/");
+        ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/plus/verifycode.asp?n=", null, map);
+        return responseHeader.bodyToImage();
+    }
 
-	/**
-	 * 登陆蓝天店主网
-	 * 
-	 * @param account
-	 * @param accountPassword
-	 * @param captcha
-	 * @param question
-	 * @param answer
-	 */
-	@Override
-	public String login(String account, String accountPassword, String captcha, String question, String answer) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Referer", "http://www2.88sxy.com/user/login/");
+    /**
+     * 登陆蓝天店主网
+     * 
+     * @param account
+     * @param accountPassword
+     * @param captcha
+     * @param question
+     * @param answer
+     */
+    @Override
+    public String login(String account, String accountPassword, String captcha, String question, String answer) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Referer", "http://www2.88sxy.com/user/login/");
 
-		HttpPost post = new HttpPost(POST_ENCODE);
-		post.add("Username", account);
-		post.add("Password", accountPassword);
-		post.add("Question", question);
-		post.add("Answer", answer);
-		post.add("Verifycode", captcha);
-		post.add("u1", null);
+        HttpPost post = new HttpPost(POST_ENCODE);
+        post.add("Username", account);
+        post.add("Password", accountPassword);
+        post.add("Question", question);
+        post.add("Answer", answer);
+        post.add("Verifycode", captcha);
+        post.add("u1", null);
 
-		ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/user/CheckUserLogin.asp", post, map);
-		return responseHeader.bodyToString(BODY_ENCODE);
-	}
+        ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/user/CheckUserLogin.asp", post, null, map);
+        return responseHeader.bodyToString(BODY_ENCODE);
+    }
 
-	/** 获取任务列表html */
-	@Override
-	public String getTaskListHtml(int page, int type, int order) {
-		page = 10 < page ? 10 : page;
-		page = page < 1 ? 1 : page;
-		type = 14 < type ? 2 : type;
-		type = type < 0 ? 2 : type;
-		order = 4 < order ? 3 : order;
-		order = order < 0 ? 3 : order;
+    /** 获取任务列表html */
+    @Override
+    public String getTaskListHtml(int page, int type, int order) {
+        page = 10 < page ? 10 : page;
+        page = page < 1 ? 1 : page;
+        type = 14 < type ? 2 : type;
+        type = type < 0 ? 2 : type;
+        order = 4 < order ? 3 : order;
+        order = order < 0 ? 3 : order;
 
-		// 第三个数字 1:淘宝任务,2:拍拍任务
-		ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/?" + type + "-" + order + "-1-0-" + page);
-		return responseHeader.bodyToString(BODY_ENCODE);
-	}
+        // 第三个数字 1:淘宝任务,2:拍拍任务
+        ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/?" + type + "-" + order + "-1-0-" + page);
+        return responseHeader.bodyToString(BODY_ENCODE);
+    }
 
-	@Override
-	public String getTaskDetail(String taskid) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Referer", "http://www2.88sxy.com/task/TaskJieShou.asp");
-		ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/TaskDetail.asp?ID=" + taskid, map);
-		return responseHeader.bodyToString(BODY_ENCODE);
-	}
+    @Override
+    public String getTaskDetail(String taskid) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Referer", "http://www2.88sxy.com/task/TaskJieShou.asp");
+        ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/TaskDetail.asp?ID=" + taskid, null, map);
+        return responseHeader.bodyToString(BODY_ENCODE);
+    }
 
-	@Override
-	public String acceptTask(Task task) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Referer", "http://www2.88sxy.com/task/");
-		ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/TaskJieShou.asp?Action=Jie&ID=" + task.getTaskId(), map);
-		return responseHeader.bodyToString(BODY_ENCODE);
-	}
+    @Override
+    public String acceptTask(Task task) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Referer", "http://www2.88sxy.com/task/");
+        ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/TaskJieShou.asp?Action=Jie&ID=" + task.getTaskId(), null, map);
+        return responseHeader.bodyToString(BODY_ENCODE);
+    }
 
-	@Override
-	public String discardTask(Task task) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Referer", "http://www2.88sxy.com/task/TaskJieShou.asp");
-		ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/TaskJieShou.asp?Action=Quit&ID=" + task.getTaskId(), map);
-		return responseHeader.bodyToString(BODY_ENCODE);
-	}
+    @Override
+    public String discardTask(Task task) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Referer", "http://www2.88sxy.com/task/TaskJieShou.asp");
+        ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/TaskJieShou.asp?Action=Quit&ID=" + task.getTaskId(), null, map);
+        return responseHeader.bodyToString(BODY_ENCODE);
+    }
 
-	@Override
-	public String getTaoBaoAccount() {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Referer", "http://www2.88sxy.com/task/BinDing.asp");
-		ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/BinDing.asp?2-0-0-0-0", map);
-		return responseHeader.bodyToString(BODY_ENCODE);
-	}
+    @Override
+    public String getTaoBaoAccount() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Referer", "http://www2.88sxy.com/task/BinDing.asp");
+        ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/BinDing.asp?2-0-0-0-0", null, map);
+        return responseHeader.bodyToString(BODY_ENCODE);
+    }
 
-	@Override
-	public String bindingBuyAccount(Task task, BuyAccount buyAccount) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Referer", "http://www2.88sxy.com/task/BinDingJieShouHao.asp?Action=BinDing&Tid=" + buyAccount.getTaobaoTid() + "&ID=" + task.getTaskId());
-		HttpPost post = new HttpPost(POST_ENCODE);
-		post.add("KS_ShopName", buyAccount.getBuyAccount());
-		post.add("Save.x", "60");
-		post.add("Save.y", "20");
-		post.add("ID", task.getTaskId());
-		post.add("Tid", buyAccount.getTaobaoTid());
-		ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/BinDingJieShouHao.asp?Action=DoSave", post, map);
-		return responseHeader.bodyToString(BODY_ENCODE);
-	}
+    @Override
+    public String bindingBuyAccount(Task task, BuyAccount buyAccount) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Referer", "http://www2.88sxy.com/task/BinDingJieShouHao.asp?Action=BinDing&Tid=" + buyAccount.getTaobaoTid() + "&ID=" + task.getTaskId());
+        HttpPost post = new HttpPost(POST_ENCODE);
+        post.add("KS_ShopName", buyAccount.getBuyAccount());
+        post.add("Save.x", "60");
+        post.add("Save.y", "20");
+        post.add("ID", task.getTaskId());
+        post.add("Tid", buyAccount.getTaobaoTid());
+        ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/BinDingJieShouHao.asp?Action=DoSave", post, null, map);
+        return responseHeader.bodyToString(BODY_ENCODE);
+    }
 
-	@Override
-	public String checkItemAddr(Task task, String itemAddr) {
-		String taskId = task.getTaskId();
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("Referer", "http://www2.88sxy.com/task/SearchLaiLuDetail.asp?UserType=2&ID=" + taskId);
+    @Override
+    public String checkItemAddr(Task task, String itemAddr) {
+        String taskId = task.getTaskId();
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("Referer", "http://www2.88sxy.com/task/SearchLaiLuDetail.asp?UserType=2&ID=" + taskId);
 
-		HttpPost post = new HttpPost(POST_ENCODE);
-		post.add("ProductUrl", itemAddr);
-		post.add("ChkProductUrl", "验证商品网址");
-		post.add("Action", "Check");
-		post.add("ID", task.getTaskId());
-		post.add("TaskType", "淘宝");
+        HttpPost post = new HttpPost(POST_ENCODE);
+        post.add("ProductUrl", itemAddr);
+        post.add("ChkProductUrl", "验证商品网址");
+        post.add("Action", "Check");
+        post.add("ID", task.getTaskId());
+        post.add("TaskType", "淘宝");
 
-		ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/SearchLaiLuDetail.asp?UserType=2&ID=" + taskId, post, map);
-		return responseHeader.bodyToString(BODY_ENCODE);
-	}
+        ResponseHeader responseHeader = httpSocket.send("http://www2.88sxy.com/task/SearchLaiLuDetail.asp?UserType=2&ID=" + taskId, post, null, map);
+        return responseHeader.bodyToString(BODY_ENCODE);
+    }
 
     @Override
     public String pay(Task task) {
-        
+
         return null;
     }
 }
